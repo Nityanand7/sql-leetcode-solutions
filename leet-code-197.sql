f1d9326493b9c4b8b05d298684aafa050ebcb3ee
Nityanand Madpathi <39838213+Nityanand7@users.noreply.github.com>
2024-09-23 15:58:31 -0500

    Adding an alternate solution for leet-code-197

--- leet-code-197.sql
+++ leet-code-197.sql
@@ -1,3 +1,19 @@
+# using window function and cte
+
+WITH cte AS
+(SELECT *, DATE_ADD(recordDate, INTERVAL - 1 DAY)
+AS yesterdays_date, LAG(recordDate) OVER(ORDER BY recordDate) AS prev_recordDate, 
+LAG(temperature) OVER(ORDER BY recordDate) AS prev_temp 
+FROM Weather)
+
+SELECT id
+FROM cte
+WHERE yesterdays_date = prev_recordDate
+AND temperature > prev_temp
+
+
+# using datediff function
+
 SELECT w1.id 
 FROM weather w1 ,weather w2 
 WHERE datediff(w1.recordDate,w2.recordDate) = 1 
