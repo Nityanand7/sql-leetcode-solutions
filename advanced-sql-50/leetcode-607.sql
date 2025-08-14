33d377ae8dac061a8458251fd9352a2b53194999
Nityanand7 <msnityanand997@gmail.com>
2025-08-13 22:12:14 -0700

    solutions for lc 1407 and 607

---
+++ advanced-sql-50/leetcode-607.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+
+SELECT name FROM SalesPerson WHERE NAME NOT IN
+(SELECT DISTINCT s.name
+FROM SalesPerson s 
+LEFT JOIN Orders o
+ON s.sales_id = o.sales_id
+LEFT JOIN Company c 
+ON o.com_id = c.com_id 
+WHERE c.name = 'RED') 
