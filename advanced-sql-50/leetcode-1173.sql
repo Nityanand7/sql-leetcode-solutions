5d504cd4111b6362695ed2d764eea34274d127a0
Nityanand7 <msnityanand997@gmail.com>
2025-08-18 18:27:31 -0700

    solutions for lc 586, 1761, 1173

---
+++ advanced-sql-50/leetcode-1173.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+
+SELECT 
+    ROUND(SUM(CASE 
+        WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END)::numeric / COUNT(*) * 100, 2) AS immediate_percentage
+FROM Delivery  
