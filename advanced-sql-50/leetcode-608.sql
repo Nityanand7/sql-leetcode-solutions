e5d7f260fb395caa8fb5aa87c9928b44c6eb86c9
Nityanand7 <msnityanand997@gmail.com>
2025-08-27 14:08:27 -0700

    solutions for lc 608 and lc 534

---
+++ advanced-sql-50/leetcode-608.sql
@@ -0,0 +1,25 @@
+-- Write your PostgreSQL query statement below
+
+SELECT id, 'Root' AS Type
+FROM Tree 
+WHERE p_id IS NULL 
+
+UNION
+
+SELECT id, 'Leaf' AS Type
+FROM Tree 
+WHERE id NOT IN (SELECT DISTINCT p_id
+                FROM Tree 
+                WHERE p_id IS NOT NULL)
+AND p_id is NOT NULL 
+
+UNION
+
+SELECT id, 'Inner' as Type 
+FROM Tree 
+WHERE id IN (SELECT DISTINCT p_id
+                FROM Tree 
+                WHERE p_id IS NOT NULL)
+AND p_id is NOT NULL
+
+
