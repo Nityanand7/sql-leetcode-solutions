4c898398a61182d7851fcef3b2bf973bee49f13f
Nityanand7 <msnityanand997@gmail.com>
2025-09-03 07:54:48 -0700

    solution for lc 1831

---
+++ advanced-sql-50/leetcode-1831.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+
+SELECT transaction_id
+FROM Transactions
+WHERE (day, amount) IN
+(SELECT day, max(amount)
+FROM Transactions
+GROUP BY day)
+ORDER BY transaction_id
