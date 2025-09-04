150de0484ca8e4a38df4606266d24569dfa7ad7b
Nityanand7 <msnityanand997@gmail.com>
2025-09-04 12:44:26 -0700

    solution for lc 1077

---
+++ advanced-sql-50/leetcode-1077.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+
+SELECT project_id, employee_id
+FROM 
+(SELECT p.project_id, p.employee_id, RANK() OVER(PARTITION BY p.project_id ORDER BY experience_years DESC) rnk
+FROM Project p 
+JOIN Employee e 
+ON p.employee_id = e.employee_id) a 
+WHERE rnk = 1
