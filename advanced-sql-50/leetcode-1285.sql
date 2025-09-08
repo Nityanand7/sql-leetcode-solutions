bf4b86a711b9543134bd37a96074869be342e89f
Nityanand7 <msnityanand997@gmail.com>
2025-09-08 08:45:38 -0700

    solution for lc 1285

---
+++ advanced-sql-50/leetcode-1285.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+
+SELECT min(log_id) as start_id, max(log_id) as end_id
+FROM
+(SELECT log_id, ROW_NUMBER() OVER(ORDER BY log_id) as num 
+FROM Logs) a 
+GROUP BY log_id - num 
+ORDER BY start_id
